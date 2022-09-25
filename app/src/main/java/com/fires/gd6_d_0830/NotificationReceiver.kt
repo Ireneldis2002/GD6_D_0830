888306ab90cf370eb339ae3a5dbacd2046c4576f
Ireneldis2002 <reneija6@gmail.com>
2022-09-25 16:19:10 +0700

    GD6_D_0830

---
+++ app/src/main/java/com/fires/gd6_d_0830/NotificationReceiver.kt
@@ -0,0 +1,13 @@
+package com.fires.gd6_d_0830
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.widget.Toast
+
+class NotificationReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent) {
+        val message = intent.getStringExtra("toastMessage")
+        Toast.makeText(context, message, Toast.LENGTH_LONG).show()
+    }
+}
